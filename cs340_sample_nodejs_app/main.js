04102e771f680ab653da56427afe2fdce12a6e9a
elboboua <elboboua@oregonstate.edu>
2019-11-25 17:44:11 +0200

    Added all the get requests for regular tables. Started work on insert requests.

--- cs340_sample_nodejs_app/main.js
+++ cs340_sample_nodejs_app/main.js
@@ -24,10 +24,12 @@ app.set('mysql', mysql);
 // app.use('/planets', require('./planets.js'));
 // app.use('/', express.static('public'));
 
+// home page that displays recipes
 app.get('/', function (req, res, next){
   res.render('recipe_main');
 });
 
+// get recipes
 app.get('/select-recipes', function (req,res,next) {
   mysql.pool.query("SELECT * FROM Recipe", function(err, result){
     if(err){
@@ -39,8 +41,59 @@ app.get('/select-recipes', function (req,res,next) {
   })
 });
 
+// get sources
+app.get('/select-sources', function (req,res,next) {
+  mysql.pool.query("SELECT * FROM Source", function(err, result){
+    if(err){
+      next(err);
+      return;
+    }
+    console.log(result);
+    res.send(JSON.stringify(result));
+  })
+});
+
+// get tags
+app.get('/select-tags', function (req,res,next) {
+  mysql.pool.query("SELECT * FROM Tag", function(err, result){
+    if(err){
+      next(err);
+      return;
+    }
+    console.log(result);
+    res.send(JSON.stringify(result));
+  })
+});
+
+// get ingredients
+app.get('/select-ingredients', function (req,res,next) {
+  mysql.pool.query("SELECT * FROM Ingredient", function(err, result){
+    if(err){
+      next(err);
+      return;
+    }
+    console.log(result);
+    res.send(JSON.stringify(result));
+  })
+});
 
 
+// post insert
+app.get('/insert-recipe', function(req, res){
+  console.log(req.body)
+  mysql.pool.query("INSERT INTO recipe (name, serving_size, cook_time, instructions) VALUES (?,?,?,?)", req.body.name, req.body.serving_size, req.body.cook_time, req.body.instructions, function(err, result){
+    if(err){
+      next(err);
+      return;
+    }
+    console.log(result);
+    res.render('recipe_main');
+  })
+});
+
+
+
+// admin stuff for pages that don't exit or errors
 app.use(function(req,res){
   res.status(404);
   res.render('404');
@@ -51,9 +104,6 @@ app.use(function(err, req, res, next){
   res.status(500);
   res.render('500');
 });
-
-
-
 
 app.listen(app.get('port'), function(){
   console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
